23fb56ac3aaa0894dcd244332737f2c96e0d1c49
manzoorWayar <softwareengineerwayar@gmail.com>
2023-04-11 12:46:18 +0430

    test successfully

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import Grid from "./Grid/Grid";
+import Node from "./Node/Node";
+import ChoiceBoard from "./ChoiceBoard/ChoiceBoard";
+import Button from "./Button/Button"; 
+import NoSolutionFoundModal from "./NoSolutionFoundModal/NoSolutionFoundModal";
+import GameDetails from "./GameDetails/GameDetails";
+import DifficultySelectionModal from "./DifficultySelectionModal/DifficultySelectionModal";
+
+export {
+  Grid,
+  Node,
+  ChoiceBoard,
+  Button, 
+  NoSolutionFoundModal,
+  GameDetails,
+  DifficultySelectionModal,
+};
